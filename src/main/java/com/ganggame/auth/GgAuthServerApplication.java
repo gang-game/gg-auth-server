3219fc87b253a9bb33d097229149d46883f761b7
ronald1-1-1 <mishakazanin@mail.ru>
2022-09-25 11:29:29 +0300

    Initial commit

---
+++ src/main/java/com/ganggame/auth/GgAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.ganggame.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgAuthServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ganggame/auth/GgAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganggame.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
